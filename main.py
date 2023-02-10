921bcd61fff5833218da652f30c9d7fde55b4579
Kamali Cooper <kjcoop0@gmail.com>
2023-02-10 01:36:05 -0500

    MOVEMENT AND BORDER DETECTION

--- main.py
+++ main.py
@@ -2,20 +2,41 @@ import pygame
 
 WIDTH, HEIGHT = 900, 500
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Game")
+pygame.display.set_caption("LEG DAY")
 
 WHITE = (255,255,255)
+BLUE = (0, 0, 225)
+GRAY = (220, 220, 220)
+
+BORDER = pygame.Rect(0, HEIGHT - 20, WIDTH, 30)
 
 FPS = 60
+VEL = 10
+
+PWIDTH , PHEIGHT = 62, 62
 
 PLAYER = pygame.image.load("assets/player.png")
+PLYR = pygame.transform.scale(PLAYER, (PWIDTH, PHEIGHT))
 
-def draw_window():
-    WINDOW.fill(WHITE)
-    WINDOW.blit(PLAYER, (0,0))
+def draw_window(play):
+    WINDOW.fill(GRAY)
+    pygame.draw.rect(WINDOW, BLUE, BORDER)
+    WINDOW.blit(PLYR, (play.x, play.y))
     pygame.display.update()
 
+def player_handle_movement(keys_pressed, play):
+    if keys_pressed[pygame.K_LEFT] and play.x - VEL + 13 > 0:
+        play.x -= VEL
+    if keys_pressed[pygame.K_RIGHT] and play.x + VEL + play.width - 12 < WIDTH:
+        play.x += VEL
+    if keys_pressed[pygame.K_UP] and play.y - VEL + 13 > 0:
+        play.y -= VEL
+    if keys_pressed[pygame.K_DOWN] and play.y + VEL + play.height - 12 < BORDER.y:
+        play.y += VEL
+
 def main():
+    play = pygame.Rect(100, 300, PWIDTH, PHEIGHT)
+
     clock = pygame.time.Clock()
 
     carryOn = True
@@ -25,7 +46,9 @@ def main():
             if event.type == pygame.QUIT:
                 carryOn = False
 
-        draw_window()
+        keys_pressed = pygame.key.get_pressed()
+        player_handle_movement(keys_pressed, play)
+        draw_window(play)
 
     pygame.quit()
 
